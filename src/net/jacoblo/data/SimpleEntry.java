a8691440bd326bfca38471834f29129ff74887c4
Jacob Lo <jacob_lo@trimble.com>
2017-09-05 17:01:43 -0500

    init hashtable

---
+++ src/net/jacoblo/data/SimpleEntry.java
@@ -0,0 +1,29 @@
+package net.jacoblo.data;
+
+import java.util.Map;
+
+public class SimpleEntry<K,V> implements Map.Entry<K,V> {
+
+  @Override
+  public K getKey() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public V getValue() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public V setValue(V arg0) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public int hashCode() {
+    
+  }
+}
---
+++ src/net/jacoblo/dataStructure/Hashtable.java
@@ -0,0 +1,66 @@
+package net.jacoblo.dataStructure;
+
+import java.util.Map;
+
+public class Hashtable<K,V> 
+{
+  private Map.Entry<K,V> table;
+  
+  public Hashtable() {
+    
+  }
+  
+  public void clear() {
+    
+  }
+  
+  public boolean contains(Object value) {
+    
+  }
+  
+  public boolean containsValue(Object value) {
+    
+  }
+  
+  public boolean containsKey(Object key) {
+    
+  }
+  
+  @Override
+  public boolean equals(Object o) {
+    
+  }
+
+  public V get(Object key) {
+    
+  }
+
+  public V put(K key, V value) {
+    
+  }
+  
+  public V remove(Object key) {
+    
+  }
+  
+  public void rehash() {
+    
+  }
+  
+  public int size() {
+    
+  }
+  
+  @Override
+  public String toString() {
+    
+  }
+  
+  public Enumeration<K> keys() {
+    
+  }
+  
+  public Enumeration<V> elements() {
+    
+  }
+}
