cb33c6e3a715f380416f54ea1b348897d6dba532
Jacob Lo <jacob_lo@trimble.com>
2017-09-12 16:35:35 -0500

    Trying to work on Longest Increasing SubSequence in Dynamic Programming way

--- src/net/jacoblo/app/MaxSubarrayProblemMain.java
+++ src/net/jacoblo/app/MaxSubarrayProblemMain.java
@@ -1,11 +1,22 @@
 package net.jacoblo.app;
 
+import java.util.ArrayList;
+
 import net.jacoblo.algorithm.MaxSubarrayProblem;
 
 public class MaxSubarrayProblemMain {
 	public static void main(String[] args) {
-		int [] a = {-2, -3, 4, -1, -2, 1, 5, -3};
-		int result = MaxSubarrayProblem.dynamicProgramming(a);
+	  // testMaxSubArrayProblem();
+	  
+	  int[] a = { 10, 22, 9, 33, 21, 50, 41, 60, 80 };
+	  ArrayList<Integer> result = MaxSubarrayProblem.LongestIncreasingSubSequence(a);
 		System.out.println(result);
+		
+	}
+	
+	public static void testMaxSubArrayProblem() {
+	   int [] a = {-2, -3, 4, -1, -2, 1, 5, -3};
+	    ArrayList<Integer> result = MaxSubarrayProblem.MaxSubarrayDynamicProgramming(a);
+	    System.out.println(result);
 	}
 }
