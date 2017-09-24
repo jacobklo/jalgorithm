a6fdb3ca8d0d446c6bb23c04a9ce08da8f05747c
JMacbook@jacoblo.net <JMacbook@jacoblo.net>
2017-09-24 15:48:01 -0500

    finish LongestCommonSubsequence

--- src/net/jacoblo/app/MaxSubarrayProblemMain.java
+++ src/net/jacoblo/app/MaxSubarrayProblemMain.java
@@ -5,14 +5,7 @@ import java.util.ArrayList;
 import net.jacoblo.algorithm.MaxSubarrayProblem;
 
 public class MaxSubarrayProblemMain {
-	public static void main(String[] args) {
-	  // testMaxSubArrayProblem();
-	  
-	  int[] a = { 10, 22, 9, 33, 21, 50, 41, 60, 80 };
-	  ArrayList<Integer> result = MaxSubarrayProblem.LongestIncreasingSubSequence(a);
-		System.out.println(result);
-		
-	}
+
 	
 	public static void testMaxSubArrayProblem() {
 	   int [] a = {-2, -3, 4, -1, -2, 1, 5, -3};
