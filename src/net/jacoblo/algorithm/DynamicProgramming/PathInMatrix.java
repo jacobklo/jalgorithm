27184836a046aba691062682a295bd7dd3a5deb3
Jacob Lo <jacob_lo@trimble.com>
2017-10-10 17:21:22 -0500

    almost finish Largest sum in path in Matrix

--- src/net/jacoblo/algorithm/DynamicProgramming/PathInMatrix.java
+++ src/net/jacoblo/algorithm/DynamicProgramming/PathInMatrix.java
@@ -14,6 +14,43 @@ Output: 4
 The longest path is 6-7-8-9. 
  */
 
+/**
+ * Largest Sum in Path in Matrix
+ * Given a N X N  matrix Matrix[N][N] of positive integers.  There are only three possible moves from a cell Matrix[r][c].
+
+1. Matrix[r+1][c]
+
+2. Matrix[r+1][c-1]
+
+3. Matrix[r+1][c+1]
+
+Starting from any column in row 0, return the largest sum of any of the paths up to row N-1.
+
+Input:
+The first line of the input contains an integer T denoting the number of test cases. The description of T test cases follows.
+The first line of each test case contains a single integer N denoting the order of matrix. Next line contains N*N integers denoting the elements of the matrix in row-major form.
+
+Output:
+Output the largest sum of any of the paths starting from any cell of row 0 to any cell of row N-1. Print the output of each test case in a new line.
+
+Constraints:
+1<=T<=20
+2<=N<=20
+1<=Matrix[i][j]<=1000 (for all 1<=i<=N && 1<=j<=N)
+
+Example:
+
+Input:
+1
+2
+348 391 618 193
+
+Output:
+1009
+
+Explanation: In the sample test case, the path leading to maximum possible sum is 391->618.  (391 + 618 = 1009)
+ */
+
 package net.jacoblo.algorithm.DynamicProgramming;
 
 public class PathInMatrix {
@@ -22,9 +59,30 @@ public class PathInMatrix {
                     { 5, 3, 8},
                     { 4, 6, 7}};
     
-    
     int result = longestPathInMatrixDP(mat);
     System.out.println(result);
+    
+    int [][] sumMat = { { 348, 391},
+                        { 618, 193}};
+    
+    int sumResult = largestSumPathInMatrixDP(sumMat);
+    System.out.println(sumResult);
+  }
+  
+  public static int largestSumPathInMatrixDP(int[][] mat) {
+    if (mat == null) return 0;
+    
+    int[][] moves = { { 1, -1},
+                    { 1, 0},
+                    {1, 1}};
+    
+    int[][] result = new int[mat[0].length+1][mat.length];
+    
+    for (int i = 0 ; i < mat[0].length ; i++) {
+      result[1][i] = mat[0][i];
+    }
+    
+    return 0;
   }
   
   public static int longestPathInMatrixDP(int[][] mat) {
