0dadc3a97b5b33d56202f9400c5f13cc3ef2ac84
JMacbook@jacoblo.net <JMacbook@jacoblo.net>
2017-10-16 22:24:09 -0500

    finish Preorder to Postorder

--- src/net/jacoblo/data/Node.java
+++ src/net/jacoblo/data/Node.java
@@ -1,6 +1,6 @@
 package net.jacoblo.data;
 
-import net.jacoblo.dataStructure.Tree.BinarySearchTree;
+import java.util.ArrayList;
 
 public class Node<E extends Object & Comparable<E>> implements Comparable<Node<E>>{
 	private String name;
@@ -79,12 +79,90 @@ public class Node<E extends Object & Comparable<E>> implements Comparable<Node<E
 		result += "(" + ( left != null ? "/" : " ") + value + ( right != null ? "\\" : " " ) + ") ";
 		return result;
 	}
+	/**
+	 * http://practice.geeksforgeeks.org/problems/preorder-to-postorder/0
+	 * Medium
+	 * Preorder to Postorder
+Show Topic Tags           Amazon
+Given an array representing preorder traversal of BST, print its postorder traversal. 
+
+Input:
+The first line of input contains an integer T denoting the number of test cases.
+The first line of each test case is N,N is the size of array.
+The second line of each test case contains N input A[i].
+
+Output:
+Postorder traversal of the given preorder traversal is printed. Otherwise 'NO' is printed.
+
+Constraints:
+1 <=T<= 100
+1 <= N <= 1000
+1 <= arr[i] <= 1000
+
+Example:
+
+Input:
+3
+5
+40 30 35 80 100
+8
+40 30 32 35 80 90 100 120
+8
+7  9 6 1 4 2 3 40
+
+Output:
+35 30 100 80 40
+35 32 30 120 100 90 80 40
+NO
+
+	 */
 	
 	public static <E extends Object & Comparable<E>> Node<E> convertFromArrayToBSTPreorder(E[] array) {
     if (array == null || array.length <= 0 ) return null;
     
+    Node<E> current = new Node<>(null,array[0]);
     
+    try {
+    for (int i = 1 ; i < array.length ; i++) {
+    	Node<E> newNode = new Node<>(null, array[i]);
+    	if ( current.getValue().compareTo(array[i]) > 0 ) {
+    		current.setLeft(newNode);
+    		newNode.setParent(current);
+    		current = newNode;
+    	}
+    	else {
+    		while(current.getParent() != null && current.getParent().getRight() != null) {
+    			current = current.getParent();
+    		} 
+    		current = current.getParent();
+    		current.setRight(newNode);
+    		newNode.setParent(current);
+    		current = newNode;
+    	}
+    }
+    
+    }catch(Exception e) {
+    	return null;
+    }
+    Node<E> treeRoot = current;
+    while(treeRoot.getParent() != null) {
+    	treeRoot = treeRoot.getParent();
+    }
+    
+    return treeRoot;
   }
+	
+	public static <E extends Object & Comparable<E>> ArrayList<E> convertFromBSTtoArrayPostorder(ArrayList<E> array, Node<E> tree) {
+		if (tree == null ) return array;
+		
+		ArrayList<E> result = new ArrayList<>();
+		result.addAll(array);
+		result.addAll(convertFromBSTtoArrayPostorder(array, tree.getLeft()));
+		result.addAll(convertFromBSTtoArrayPostorder(array, tree.getRight()));
+		result.add(tree.getValue());
+		
+		return result;
+	}
 	
 	public static <E extends Object & Comparable<E>> void swap(Node<E> a, Node<E> b) {
 		if (a == null || b == null) return;
