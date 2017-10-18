aafa2ada6860c26969740f238a4a77d58e52932e
Jacob Lo <jacob_lo@trimble.com>
2017-10-18 11:15:47 -0500

    add comment

--- src/net/jacoblo/algorithm/DynamicProgramming/RegionsInMatrix.java
+++ src/net/jacoblo/algorithm/DynamicProgramming/RegionsInMatrix.java
@@ -1,3 +1,45 @@
+
+/**
+ * http://practice.geeksforgeeks.org/problems/length-of-largest-region-of-1s/0
+ * Medium
+ * Length of largest region of 1's
+Show Topic Tags           Amazon
+Consider a matrix with rows and columns (n,m), where each cell contains either a ‘0’ or a ‘1’ and any cell containing a 1 is called a filled cell. Two cells are said to be connected if they are adjacent to each other horizontally, vertically, or diagonally .If one or more filled cells are connected, they form a region. Your task is to  find the length of the largest region.
+
+Examples:
+
+Input : M[][5] = { 0 0 1 1 0
+                   1 0 1 1 0
+                   0 1 0 0 0
+                   0 0 0 0 1 }
+Output : 6 
+Ex: in the following example, there are 
+2 regions one with length 1 and the other as 6.
+so largest region : 6
+
+
+Input:
+The first line of input will be the no of test cases T then T test cases follow. The first line of each test case contains Two space separated integers n and m. Then in the next line are the nxm inputs of the matrix A separated by space .
+
+Output:
+The output in the expected output will be the length of the largest region formed.
+
+Constraints:
+1<=T<=100
+1<=n,m<=50
+0<=A[][]<=1
+
+Example:
+Input:
+2
+3 3
+1 1 0 0 0 1 1 0 1
+1 3
+1 1 1
+Output:
+4
+3
+ */
 package net.jacoblo.algorithm.DynamicProgramming;
 
 public class RegionsInMatrix {
